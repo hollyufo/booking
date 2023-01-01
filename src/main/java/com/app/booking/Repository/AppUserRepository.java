2572b6d39b07c12bda449b64156d03cf3cfb8fee
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2023-01-01 21:56:30 +0000

    created user repository

--- src/main/java/com/app/booking/Repository/AppUserRepository.java
+++ src/main/java/com/app/booking/Repository/AppUserRepository.java
@@ -1,4 +1,17 @@
 package com.app.booking.Repository;
 
-public class AppUserRepository {
+
+import com.app.booking.Entities.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+    AppUser findByEmail(String email);
+
+    // list all the managers
+    List<AppUser> findAllByRole(String role);
+    // update the role of a user
+    AppUser findByRole(String role);
+
 }
